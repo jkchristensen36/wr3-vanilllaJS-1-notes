7ecb6942c88920822f7fc702823f58bbbbb7cb0d
Jordan Christensen <jchristensen@Jordans-MacBook-Pro.local>
2020-07-14 11:10:04 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // DOM
-
+console.log('hello world')
+console.log(window)
 // Accessing DOM Elements
-
+const section = document.getElementById('awesome-section')
+console.log('section', section)
 // Node Properties
 // innerText, innerHTML, className, classList, style, value
 
